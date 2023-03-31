56abb1fc3ffd5127eef8ca2712e27dcad4d2314a
JohnnyWong-hub <61719644+JohnnyWong-hub@users.noreply.github.com>
2023-03-31 15:30:07 +0800

    Create app_main.c

---
+++ app_main.c
@@ -0,0 +1,32 @@
+
+
+
+int main(int argc, char  **argv)
+{
+    int choose;
+    while(1)
+    {
+        
+        printf("---------------------------------------\n");
+        printf("enter you choose\n");
+        printf("1 : 功能1 2：功能2 3：功能3\n");
+        printf("---------------------------------------\n");
+        scanf("%d\n", &choose);
+        switch(choose)
+        {
+            case 1:
+                //func1
+                break;
+            case 2:
+                //fun2
+                break;
+            case 3:
+                //fun3
+                break;
+            default:
+                break;
+        }
+    
+    }
+    return 0;
+}
